3fb3dbdbc2c208ab16da1cc78f4ba80ac9e2bda2
fabry-js <vivaldifabrizio10@gmail.com>
2023-10-30 13:28:23 +0100

    feat: :sparkles: add transformKeys

--- algorithms/src/lib/algorithms.spec.ts
+++ algorithms/src/lib/algorithms.spec.ts
@@ -1,7 +1,32 @@
-import { algorithms } from './algorithms';
+import { transformKeys } from './algorithms'; // Replace with the correct path
 
-describe('algorithms', () => {
-  it('should work', () => {
-    expect(algorithms()).toEqual('algorithms');
+describe('transformKeys', () => {
+  it('transforms keys asynchronously according to the async callback', async () => {
+    const inputObject = { a: 1, b: 2, c: 3 };
+    const asyncTransformCallback = async (key: string) => key.toUpperCase();
+    const a = transformKeys(inputObject, (ok) => ok.toUpperCase());
+    const result = await transformKeys(inputObject, asyncTransformCallback);
+    expect(result).toEqual({ A: 1, B: 2, C: 3 });
+  });
+
+  it('transforms keys with different types asynchronously', async () => {
+    const inputObject = { age: 30, isStudent: true, has_spaces: 'yes' };
+    const asyncTransformCallback = async (key: string) => key.replace('_', ' ');
+    const result = await transformKeys(inputObject, asyncTransformCallback);
+    expect(result).toEqual({ age: 30, isStudent: true, 'has spaces': 'yes' });
+  });
+
+  it('does not modify the original object', async () => {
+    const inputObject = { a: 1, b: 2 };
+    const asyncTransformCallback = async (key: string) => key.toUpperCase();
+    const result = await transformKeys(inputObject, asyncTransformCallback);
+    expect(inputObject).toEqual({ a: 1, b: 2 });
+  });
+
+  it('handles an empty object asynchronously', async () => {
+    const inputObject = {};
+    const asyncTransformCallback = async (key: string) => key.toUpperCase();
+    const result = await transformKeys(inputObject, asyncTransformCallback);
+    expect(result).toEqual({});
   });
 });
--- algorithms/src/lib/algorithms.ts
+++ algorithms/src/lib/algorithms.ts
@@ -1,3 +1,56 @@
-export function algorithms(): string {
-  return 'algorithms';
+/**
+ * @name transformKeys
+ *
+ * @description
+ * Transforms an object's keys based on a provided callback function, which can be either async or synchronous.
+ *
+ * @param o
+ * The object to be operated on.
+ *
+ * @param transformCallback
+ * The callback function that transforms each key. It must return a string as the keys are expected to be strings.
+ *
+ * @returns
+ * A promise (if the callback is async) or a new object with transformed keys (if the callback is synchronous).
+ */
+export function transformKeys<T>(
+  o: Record<string, T>,
+  transformCallback: (originalKey: string) => string
+): Record<string, T>;
+
+export function transformKeys<T>(
+  o: Record<string, T>,
+  transformCallback: (originalKey: string) => Promise<string>
+): Promise<Record<string, T>>;
+
+// Implementation of the overloaded functions
+export async function transformKeys<T>(
+  o: Record<string, T>,
+  transformCallback: (originalKey: string) => string | Promise<string>
+): Promise<Record<string, T>> {
+  // Create an array to store transformation promises.
+  const transformationPromises = Object.keys(o).map(async (key) => {
+    // Ensure that the key is an own property of the object using Object.prototype.hasOwnProperty.
+    if (Object.prototype.hasOwnProperty.call(o, key)) {
+      // Apply the transformation callback to the key.
+      const newKey = await transformCallback(key);
+      return [newKey, o[key]];
+    }
+
+    return undefined;
+  });
+
+  // Use Promise.all to resolve all transformation promises.
+  const transformedKeyValues = (
+    await Promise.all(transformationPromises)
+  ).filter((result): result is [string, T] => result !== undefined); // Filter out undefined results
+
+  // Convert the array of key-value pairs into a new object.
+  const newObject: Record<string, T> = {};
+  for (const [newKey, value] of transformedKeyValues) {
+    newObject[newKey] = value;
+  }
+
+  // Return the new object with transformed keys.
+  return newObject;
 }
